d7b3db430f543aef4d0d70dc53ffce3e1b0fa865
贾子权 <JZQ@jzqmacbook-pro.local>
2018-04-17 11:06:08 +0800

    1

---
+++ gitDemo/gitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitDemo
+//
+//  Created by 贾子权 on 2018/4/17.
+//  Copyright © 2018年 贾子权. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
